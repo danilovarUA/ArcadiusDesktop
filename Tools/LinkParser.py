6897cf06458f89fc9a7c7a19b8a517aa4d7323f2
Danilo <dan.varushchik@gmail.com>
2020-07-20 18:30:59 +0300

    adding region to linkparser

--- Tools/LinkParser.py
+++ Tools/LinkParser.py
@@ -1,9 +1,11 @@
+import re
+
+
 def search_text_for_links(text):
     link_type_to_pattern = {'alliance': r'l\+k://alliance\?\d+&\d+',
                             'player': r'l\+k://player\?\d+&\d+',
-                            'habitat': r'l\+k://coordinates\?\d+,\d+&\d+'}
-    # TODO add region
-
+                            'habitat': r'l\+k://coordinates\?\d+,\d+&\d+',
+                            'region': r'l\+k://region\?\d+,\d+&\d+'}
     links_by_type = {}
     for link_type in link_type_to_pattern.keys():
         links_by_type[link_type] = re.findall(link_type_to_pattern[link_type], text, flags=0)
